df38e371b4725a2374cb56e76a7045f693445d6c
artem sitnikov <80666405699@mail.ru>
2016-05-11 13:01:49 +0300

    new repository

---
+++ src/main/java/uits/jv1604/hometasks/TmpFile.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uits.jv1604.hometasks;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TmpFile {
+    public static void main(String[] args) {
+        System.out.println("проба русского языка");
+    }
+}
